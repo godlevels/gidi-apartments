22991a89bfedb9e9ac0abb3af9c0ffd757c70608
CryptoVmD <93841754+godlevels@users.noreply.github.com>
2023-02-09 16:12:06 +0100

    new updates

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import HomeDetails from './pages/HomeDetails'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+const router = createBrowserRouter([
+  { 
+    path: '/',
+    element: <Home />
+  },
+
+  {
+    path: '/home/:id',
+    element: <HomeDetails />
+  },
+])
+
+function App() {
+  
+  return (
+    <div>
+      <Navbar />
+      <RouterProvider router={router} />
+      <Footer />
+    </div> 
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rooms from '../components/Rooms'
+import BookForm from '../components/BookForm'
+const Home = () => {
+    return (
+        <>
+            <BookForm />
+            <Rooms />   
+        </>
+    )
+}
+
+export default Home 
