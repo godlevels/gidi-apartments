49d6b5a061fed1361dfd19fb5975fbd4cae1b235
CryptoVmD <93841754+godlevels@users.noreply.github.com>
2023-03-07 10:20:36 +0100

    resolves

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -3,22 +3,41 @@ import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/effect-fade'
 import { EffectFade, Autoplay } from 'swiper'
-import Img1 from '../assets/img/heroSlider/1.jpg'
+import Img1 from '../assets/img/heroSlider/14.jpg'
 // import Img2 from '../assets/img/heroSlider/2.jpg'
 // import Img3 from '../assets/img/heroSlider/3.jpg'
 
 
 const data = [
     {
-        title: 'Discover The Most Suitable Property Today',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor facere illo maiores repellendus laudantium?',
+        // title: 'Discover The Most Suitable Property Today',
+        desc: 'Find a variety of properties that suit you very easily, forget all difficulties in finding a residence for you.',
         btnText: 'Explore'
     },
 ]
 
 const HeroSlider = () => {
     return (
-        // <Swiper modules={[EffectFade, Autoplay]} effect={'fade'} loop={true} autoplay={{delay: 3000, disableOnInteraction: false,}} className='heroSlider h-[600px] lg:h-[860px]'>
+        <div>
+            <div className='mx-auto container h-[600px] lg:h-[500px]'>
+                <div className='h-full relative flex flex-col lg:flex-row lg:justify-between lg:gap-x-16 items-center'>
+                    <div className='z-20'>
+                        <img className='h-[600px]' src={Img1} alt="" />
+                    </div>
+                    <div className='text-center lg:text-left'>
+                        <h1 className='text-[40px] font-semibold leading-10'>Discover <br /> The Most Suitable Property Today</h1>
+                        {data.map((s, i)=> {
+                            const {desc, btnText} = s
+                            return <div key={i}>
+                                <p className='text-black text-[15px] my-4'>{desc}</p>
+                                <button className='bg-accent text-white text-[20px] py-2 px-5 lg:my-0'>{btnText}</button>
+                            </div>
+                        })}
+                    </div>
+                </div>
+            </div>
+
+            {/* // <Swiper modules={[EffectFade, Autoplay]} effect={'fade'} loop={true} autoplay={{delay: 3000, disableOnInteraction: false,}} className='heroSlider h-[600px] lg:h-[860px]'>
         //     {slides.map((s, i)=> {
         //         const {title, bg,} = s
         //         return <SwiperSlide key={i} className='h-full bg-pink-400 relative flex justify-center items-center'>
@@ -35,15 +54,7 @@ const HeroSlider = () => {
         //             </div>
         //         </SwiperSlide>
         //     })}
-        // </Swiper>
-
-        <div>
-            <div className='flex'>
-                <div>
-                    <img src={Img1} alt="" />
-                </div>
-                <div>2</div>
-            </div>
+        // </Swiper> */}
         </div>
     )
 }
